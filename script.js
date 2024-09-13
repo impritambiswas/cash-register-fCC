05e16d3b1f614fae69d904b610def1aa8b93f244
Pritam Biswas <121305448+impritambiswas@users.noreply.github.com>
2024-09-12 20:18:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+const values = [
+  { 'PENNY': 0.01 },
+  { 'NICKEL': 0.05 },
+  { 'DIME': 0.1 },
+  { 'QUARTER': 0.25 },
+  { 'ONE': 1 },
+  { 'FIVE': 5 },
+  { 'TEN': 10 }
+  {'TWENTY': 20},
+  {'ONE HUNDRED': 100}
+]
+
+const cash = document.getElementById('cash');
+const changeDue = document.getElementById('change-due');
+const purchaseBtn = document.getElementById('purchase-btn');
+
+
+const giveChange = () => {
+  if (cash.value === '' || parseFloat(cash.value) < price) {
+    alert('Customer does not have enough money to purchase the item');
+  } else {
+    changeValidation();
+  }
+}
+
+const changeValidation = () => {
+  const valueForMoney = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100].reverse();
+  const cashInDrawer = cid.slice().map(val => val[1]).reverse();
+  const monetery = cid.slice().map(key => key[0]).reverse();
+  let reqMoney = 0;
+  let cashAvailArr = new Array(9);
+
+  for (let i = 0; i < cashAvailArr.length; i++) {
+    cashAvailArr[i] = cashInDrawer[i] * valueForMoney[i];
+  }
+
+  const cashAvail = cashAvailArr.reduce((acc, curr) => acc + curr);
+  let changeAmount = parseFloat(cash.value) - price;
+
+  if (changeAmount === 0) {
+    changeDue.textContent = 'No change due - customer paid with exact cash';
+    return;
+  } else if (changeAmount > cashAvail) {
+    changeDue.textContent = 'Status: INSUFFICIENT_FUNDS';
+    return;
+  } else {
+    for (let i = 0; i < valueForMoney.length; i++) {
+      while (changeAmount >= valueForMoney[i]) {
+        changeAmount -= valueForMoney[i];
+        reqMoney += valueForMoney[i];
+        changeDue.innerHTML += `<p>${monetery[i]} : ${reqMoney / valueForMoney[i]}</p>`;
+      }
+      reqMoney = 0;
+    }
+  }
+  purchaseBtn.addEventListener('click', giveChange);
