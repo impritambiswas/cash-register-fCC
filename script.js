6bbc29c0221665a52f3583ea2d12768344c1c2be
Pritam Biswas <121305448+impritambiswas@users.noreply.github.com>
2024-09-26 08:25:15 +0000

    Almost a reset to the js file

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 const values = [
-  { 'PENNY': 0.01 },
-  { 'NICKEL': 0.05 },
-  { 'DIME': 0.1 },
-  { 'QUARTER': 0.25 },
-  { 'ONE': 1 },
-  { 'FIVE': 5 },
-  { 'TEN': 10 },
-  { 'TWENTY': 20 },
-  { 'ONE HUNDRED': 100 }
+  ['PENNY', 0.01],
+  ['NICKEL', 0.05],
+  ['DIME', 0.1],
+  ['QUARTER', 0.25],
+  ['ONE', 1],
+  ['FIVE', 5],
+  ['TEN', 10],
+  ['TWENTY', 20],
+  ['ONE HUNDRED', 100]
 ]
 
 const cash = document.getElementById('cash');
@@ -30,7 +30,6 @@ const reset = () => {
   cash.value = '';
   changeDue.innerHTML = ``;
 }
-
 
 const giveChange = () => {
   if (cash.value === '' || parseFloat(cash.value) < price) {
@@ -43,27 +42,6 @@ const giveChange = () => {
 const changeValidation = (cash) => {
   const valueForMoney = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100].reverse();
   const cashInDrawer = cid.slice().map(val => val[1]).reverse();
-  const monetery = cid.slice().map(key => key[0]).reverse();
-  let reqMoney = 0;
-  let cashAvailArr = new Array(9);
 
-  for (let i = 0; i < cashAvailArr.length; i++) {
-    cashAvailArr[i] = cashInDrawer[i] * valueForMoney[i];
-  }
-
-  const cashAvail = cashAvailArr.reduce((acc, curr) => acc + curr);
-  let changeAmount = parseFloat(cash.value) - price;
-
-  if (changeAmount === 0) {
-    changeDue.textContent = 'No change due - customer paid with exact cash';
-    return;
-  } else if (changeAmount > cashAvail) {
-    changeDue.textContent = 'Status: INSUFFICIENT_FUNDS';
-    return;
-  } else {
-    reset();
-    giveChange();
-  }
-  return console.log(cash.value);
 }
 purchaseBtn.addEventListener('click', giveChange);
